921f427899ade1adaefa83dd48e1b9c79c8348e9
nocosharold <nocosharold@gmail.com>
2021-09-02 17:05:30 +0800

    Added display Life and Score

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,9 @@
-let left_position_value = 480;
-let top_position_value = 300;
+let hero_position = {
+    x: 480,
+    y: 300
+}
 
-$(document).keydown(function(e){
-    moveHero(e);
-});
+let life_remaining = 3;
 
 /**
 *   DOCU: This function is used to move a hero space ship
@@ -13,28 +13,76 @@ $(document).keydown(function(e){
 */
 function moveHero(e){
     /* up arrow */
-    if(e.keyCode == 38 && top_position_value - 8 != 12) {
-        top_position_value = parseInt($("#hero").css("top"));
-        top_position_value -= 8;
-        $("#hero").css({ "top": top_position_value + "px" });
+    if(e.keyCode === 38 && hero_position.y - 8 != 12) {
+        hero_position.y = parseInt($("#hero").css("top"));
+        hero_position.y -= 8;
+        $("#hero").css({ "top": hero_position.y + "px" });
     }
     /* down arrow */
-    else if(e.keyCode == 40 && top_position_value - 8 != 644) {
-        top_position_value = parseInt($("#hero").css("top"));
-        top_position_value += 8;
-        $("#hero").css({ "top": top_position_value + "px" });
+    else if(e.keyCode === 40 && hero_position.y - 8 != 644) {
+        hero_position.y = parseInt($("#hero").css("top"));
+        hero_position.y += 8;
+        $("#hero").css({ "top": hero_position.y + "px" });
     }
     /* left arrow */
-    else if(e.keyCode == 37 && left_position_value - 8 != 344) {
-        left_position_value = parseInt($("#hero").css("left"));
-        left_position_value -= 8;
-        $("#hero").css({ "left": left_position_value + "px" });
+    else if(e.keyCode === 37 && hero_position.x - 8 != 344) {
+        hero_position.x = parseInt($("#hero").css("left"));
+        hero_position.x -= 8;
+        $("#hero").css({ "left": hero_position.x + "px" });
     }
     /* right arrow */
-    else if(e.keyCode == 39 && left_position_value + 8 != 888) {
-        left_position_value = parseInt($("#hero").css("left"));
-        left_position_value += 8;
-        $("#hero").css({ "left": left_position_value + "px" });
+    else if(e.keyCode === 39 && hero_position.x + 8 != 888) {
+        hero_position.x = parseInt($("#hero").css("left"));
+        hero_position.x += 8;
+        $("#hero").css({ "left": hero_position.x + "px" });
     }
-    console.log(e.keyCode);
 }
+
+/**
+*   DOCU: This function is used to display life and score
+*   Last updated at: September 2, 2021
+*   @author Harold
+*/
+function displayLifeAndScore(){
+    
+    let life_template = ``;
+
+    for(let i = 0; i < life_remaining; i++){
+        life_template += `<div id="hero_life"></div>`
+    }
+    life_template += `<h2>SCORE:100</h2>`
+    $("#life_score_container").html(life_template);
+    console.log(life_template);
+}
+
+function gameLoop(){
+    displayLifeAndScore();
+}
+
+setInterval(gameLoop, 50);
+
+$(document).keydown(function(e){
+    moveHero(e);
+    /* if(e.keyCode === 32){
+    } */
+});
+
+/**
+ * Game Over when player loses all 3 lives
+ * When Player collides with an enemy or an enemy bullet, player loses one life, then respawns in the middle left side of the game screen
+ * After respawning player has 5 to be invulnerable to enemies and bullets
+ * While invulnerable, colliding enemies or enemy bullets get automatically destroyed
+ * Enemies and enemy bullets take damage when they collide with player bullets
+ * Bullets are destroyed when they reach the end of the game screen
+ * Spawn enemies outside the right side of the game screen
+ * Spawn regular enemies in random vertical positions
+ * Boss enemies spawn in the vertical middle of the screen, then begin their movement patterns after they reach the center of the boss area
+ * Spawn a random Boss every score of 1500
+ * Boss enemies do not go beyond the boss area (approx 1/4 of the game screen)
+ * When a boss enemy is active, do not spawn new regular enemies until the boss is destroyed. Current regular enemies stay until they leave the game screen
+ * Increase the movement of Regular enemies, every 1000 pts
+ * Increase the spawn rate of Regular enemies, every 200 pts
+ * e.g. enemy_count = (score == 0) ? 1 : Math.round(score/200)
+ ** Player Start with 3 lives
+ ** Game Screen follows 4:3 ratio
+*/
